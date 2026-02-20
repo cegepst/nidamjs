6ae06133edcf4c91d6f0d81efd2070dba734c8d5
Zach <zach160jg@gmail.com>
2026-02-20 11:18:59 -0500

    feat: created an example nidam.config.js file

---
+++ examples/csr/nidam.config.js
@@ -0,0 +1,9 @@
+export default {
+    refreshTimeout: 300,
+    modalContainer: "#target",
+    // example for future UI config
+    ui: {
+        theme: "dark",
+        taskbar_size: "small",
+    },
+};
