7558795a9f11ef3fc655409ee6ac9832db2c658f
Zach <zach160jg@gmail.com>
2026-02-24 14:26:11 -0500

    refactor: added comments and changed file names

--- examples/csr/app.js
+++
@@ -1,4 +0,0 @@
-import { initNidamApp } from '../../dist/nidam.es.js';
-import config from '../shared/nidam.config.json' with { type: 'json' };
-
-initNidamApp(config);
---
+++ examples/csr/example.js
@@ -0,0 +1,5 @@
+// Custom config example for NidamJS
+import { initNidamApp } from '../../dist/nidam.es.js';
+import config from '../shared/example.config.json' with { type: 'json' };
+
+initNidamApp(config);
