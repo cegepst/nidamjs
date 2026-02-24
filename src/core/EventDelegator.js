56a56c9223c293dba08a02741af0f8e9a0ad6384
Zach <zach160jg@gmail.com>
2026-02-24 14:36:07 -0500

    refactor: removed useless validation

--- src/core/EventDelegator.js
+++ src/core/EventDelegator.js
@@ -1,12 +1,8 @@
 export default class EventDelegator {
   #handlers = new Map();
-  /** @type {Document | HTMLElement} */
   #root;
   #eventListeners = new Map();
 
-  /**
-   * @param {Document | HTMLElement} root
-   */
   constructor(root = document) {
     this.#root = root;
     this.#initDelegation();
