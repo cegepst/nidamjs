24b18124154cd4fa6284d237e42ce4228e3daa33
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-02-27 12:40:29 -0500

    eos-290-refactor: make the window styles separate in the library

--- examples/ssr/server/templates/windowShell.js
+++ examples/ssr/server/templates/windowShell.js
@@ -1,17 +1,15 @@
 export function renderWindowShell({ title, content, defaultSnap = true }) {
-  const snapAttr = defaultSnap ? "data-default-snap" : "";
+    const snapAttr = defaultSnap ? "data-default-snap" : "";
 
-  return `
-  <div class="window window-shell animate-appearance" ${snapAttr}>
-    <div data-bar class="window-bar">
-      <strong class="window-title">${title}</strong>
-      <div class="window-actions">
-        <button data-maximize class="mini-btn" title="Maximize"><i class="not-italic">[]</i></button>
-        <button data-close class="mini-btn" title="Close"><i class="not-italic">x</i></button>
-      </div>
+    return `
+  <div nd-window class="window" ${snapAttr}>
+    <div nd-window-header data-bar>
+      <span>${title}</span>
+      <button nd-window-button data-maximize title="Maximize">[ ]</button>
+      <button nd-window-button data-close title="Close">X</button>
     </div>
-    <div class="window-content-scrollable window-content-shell">
-      <div class="window-content">${content}</div>
+    <div nd-window-content>
+        ${content}
     </div>
   </div>`;
 }
