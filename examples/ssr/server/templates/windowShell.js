523baf62a26cffbf585c3768ae9583d50105713f
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-02-27 13:53:33 -0500

    eos-290-style: change style of buttons

--- examples/ssr/server/templates/windowShell.js
+++ examples/ssr/server/templates/windowShell.js
@@ -3,8 +3,8 @@ export function renderWindowShell({ title, content, defaultSnap = true }) {
   <div nd-window class="window">
     <div nd-window-header data-bar>
       <span>${title}</span>
-      <button nd-window-button data-maximize title="Maximize">[ ]</button>
-      <button nd-window-button data-close title="Close">X</button>
+      <button nd-window-button="maximize" data-maximize title="Maximize">[ ]</button>
+      <button nd-window-button="close" data-close title="Close">X</button>
     </div>
     <div nd-window-content>
         ${content}
